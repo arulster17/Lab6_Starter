0586fc94ab74d65621784ac1e17ac09393c15eeb
Arul Mathur <arulster17@gmail.com>
2025-05-14 03:13:53 -0700

    done with explore

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -64,6 +64,7 @@ function saveRecipesToStorage(recipes) {
 	// B1. TODO - Complete the functionality as described in this function
 	//            header. It is possible in only a single line, but should
 	//            be no more than a few lines.
+	localStorage.setItem('recipes', JSON.stringify(recipes));
 }
 
 /**
@@ -72,6 +73,8 @@ function saveRecipesToStorage(recipes) {
  */
 function initFormHandler() {
 	// B2. TODO - Get a reference to the <form> element
+	let form = document.querySelector('form');
+
 	// B3. TODO - Add an event listener for the 'submit' event, which fires when the
 	//            submit button is clicked
 	// Steps B4-B9 will occur inside the event listener from step B3
@@ -84,9 +87,31 @@ function initFormHandler() {
 	// B8. TODO - Append this new <recipe-card> to <main>
 	// B9. TODO - Get the recipes array from localStorage, add this new recipe to it, and
 	//            then save the recipes array back to localStorage
+	form.addEventListener('submit', function(event) {
+		event.preventDefault();
+		let formData = new FormData(form);
+		let recipeData = {};
+		for(const [k, v] of formData.entries()) {
+			recipeData[k] = v;
+		}
+		let recipeElement = document.createElement('recipe-card');
+		recipeElement.data = recipeData;
+		document.querySelector('main').append(recipeElement);
+		
+		let recipesList = getRecipesFromStorage();
+		recipesList.push(recipeData);
+		saveRecipesToStorage(recipesList);
+	});
+
 	// B10. TODO - Get a reference to the "Clear Local Storage" button
+	const clsButton = document.querySelector('button[class="danger"]');
+
 	// B11. TODO - Add a click event listener to clear local storage button
 	// Steps B12 & B13 will occur inside the event listener from step B11
 	// B12. TODO - Clear the local storage
 	// B13. TODO - Delete the contents of <main>
+	clsButton.addEventListener('click', function() {
+		localStorage.clear();
+		document.querySelector('main').innerHTML = '';
+	})
 }
